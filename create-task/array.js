c01846cc377f4b753422ba0bebfd14077a0e5a2c
michaelv24 <mvsch20@gmail.com>
2023-02-17 10:14:55 -0500

    array updates

--- create-task/array.js
+++ create-task/array.js
@@ -13,4 +13,58 @@ image:"",
     brand: "bounty",
     image:"",
 }
+{
+    name:"plates",
+    type:"utilities",
+    brand:"Dinnerware",
+    image:"",
+
+}
+{
+    name:"forks",
+    type:"utilities",
+    brand:"Dinnerware",
+    image:"",
+
+}
+{
+    name:"spoons",
+    type:"utilities",
+    brand:"Dinnerware",
+    image:"",
+}
+{
+    name:"Treasure Island",
+    type:"Books",
+    filter:"mature",
+    brand:"BarneisNoble",
+    image:"",
+}
+{
+    name:"Vinland Saga",
+    type:"Books",
+    filter:"mature",
+    brand:"BarneisNoble",
+    image:"",
+}
+{
+    name:"Bob the Builder's Children Book",
+    type:"children books",
+    filter:"children",
+    brand:"Bob",
+    image:"",
+}
+{
+    name:"Caterpillar",
+    type:"children books",
+    filter:"children",
+    brand:"Bob'children books",
+    image:"",
+}
+{
+    name:"Naruto",
+    type:"Books",
+    filter:"mature",
+    brand:"BarneisNoble",
+}
 ];
