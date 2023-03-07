b8eef3db1e3d0d2ae52357c97d6f976d00d9f82a
michaelv24 <mvsch20@gmail.com>
2023-03-07 07:33:00 -0500

    done

--- create-task/main.js
+++ create-task/main.js
@@ -1,19 +1,71 @@
-import "styles./style.css";
+import "./styles/style.css";
 import { array } from "./array.js";
 
 const DOMSelectors = {
-  buttonUtil: document.getElementById("btnUtil"),
-  buttonBooks: document.getElementById("btnBooks"),
-  buttonToys: document.getElementById("btnToys"),
-  storeFront: document.querySelector("main"),
+  buttonUtil: document.querySelector(".btnUtil"),
+  buttonBook: document.querySelector(".btnBooks"),
+  buttonToy: document.querySelector(".btnToy"),
+  storeFront: document.querySelector(".main"),
   name: document.querySelector(".name"),
   item: document.querySelector(".brand"),
   display: document.querySelector(".img"),
   remove: document.getElementsByClassName("remove"),
 };
 
-DOMSelectors.button;
+const chooseUtilities = function () {
+  array
+    .filter((theArr) => theArr.type === "utilities")
+    .forEach((array) =>
+      DOMSelectors.display.insertAdjacentHTML(
+        "beforeend",
+        `<div class="text"><img class="img" src=${array.image}><img><p>${array.name}</p>
+        <p>${array.type}</p> </div>`
+      )
+    );
+};
+const chooseBooks = function () {
+  array
+    .filter((theArr) => theArr.type === "Books")
+    .forEach((array) =>
+      DOMSelectors.display.insertAdjacentHTML(
+        "beforeend",
+        `<div class="text"><img class="img" src=${array.image}><img><p>${array.name}</p>
+        <p>${array.type}</p> </div>`
+      )
+    );
+};
+const chooseToys = function () {
+  array
+    .filter((theArr) => theArr.type === "Toys")
+    .forEach((array) =>
+      DOMSelectors.display.insertAdjacentHTML(
+        "beforeend",
+        `<div class="text"><img class="img" src=${array.image}><img><p>${array.name}</p>
+        <p>${array.type}</p> </div>`
+      )
+    );
+};
 
-function removeItem(i) {
-  i.parentElement.remove();
-}
+DOMSelectors.buttonUtil.addEventListener("click", function () {
+  console.log(DOMSelectors.display);
+  removeDiv();
+  chooseUtilities();
+});
+DOMSelectors.buttonBook.addEventListener("click", function () {
+  console.log(DOMSelectors.display);
+  removeDiv();
+  chooseBooks();
+});
+DOMSelectors.buttonToy.addEventListener("click", function () {
+  console.log(DOMSelectors.display);
+  removeDiv();
+  chooseToys();
+});
+
+// function removeDiv(e) {
+//   e.parentElement.remove();
+// }
+
+const removeDiv = function () {
+  DOMSelectors.display.innerHTML = "";
+};
